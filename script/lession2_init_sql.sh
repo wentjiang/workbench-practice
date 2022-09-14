8fb3f5e2b37024fe5f290557a6b70b4ca778fde0
wentjiang <wentjiang@sina.com>
2022-09-14 21:37:54 +0800

    update

--- script/lession2_init_sql.sh
+++ script/lession2_init_sql.sh
@@ -11,11 +11,3 @@ init_sql_file_name="init.sql"
 
 init_table_cmd="mysql -h 127.0.0.1 -P 26100 -u root -p123456 -D minibank < ${base_path}${init_sql_file_name}"
 eval "${init_table_cmd}"
-
-#for sql_file in $sql_files; do
-#  if [ "$sql_file" != "${init_sql_file_name}" ]; then
-#    cmd="mysql -h 127.0.0.1 -u root -ppassword -D workbench < $base_path$sql_file"
-#    echo "$cmd"
-#    eval "$cmd"
-#  fi
-#done
